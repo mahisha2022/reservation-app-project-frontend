0e7076a78ba68b72edfb6dd3fb0d9b75b22e71a3
austinalex1995 <aealex1995@gmail.com>
2023-03-21 17:14:59 -0400

    updated default routing and updated login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './pages/login/login.component.html',
+  styleUrls: ['./pages/login/login.component.css']
 })
 export class AppComponent {
   title = 'reservation-app-project-frontend';
