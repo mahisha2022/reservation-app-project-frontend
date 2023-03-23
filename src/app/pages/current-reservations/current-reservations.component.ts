db9a999ddf5b6e67b3029c0a0589c293194c7884
austinalex1995 <aealex1995@gmail.com>
2023-03-23 16:13:55 -0400

    binded data to current-reservations and displayed the data

--- src/app/pages/current-reservations/current-reservations.component.ts
+++ src/app/pages/current-reservations/current-reservations.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Reservation } from 'src/app/models/Reservation';
 
 @Component({
   selector: 'app-current-reservations',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./current-reservations.component.css']
 })
 export class CurrentReservationsComponent {
+
+  /**
+   * Member Variables
+   */
+  
+  currentReservations : Reservation[] = [
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "5:00 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "5:30 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "6:00 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "6:30 PM"
+    }
+  ];
+
+
+
+  /**
+   * Constructor
+   */
+
+  constructor() { }
+
+
+
+  /**
+   * Class Methods
+   */
 
 }
