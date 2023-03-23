ae9efb09a5e13fd20f9210b8871a0f5ab0a4939f
austinalex1995 <aealex1995@gmail.com>
2023-03-23 14:56:33 -0400

    added data binding for restaurant list with test data

--- src/app/components/restaurant-list/restaurant-list.component.ts
+++ src/app/components/restaurant-list/restaurant-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Restaurant } from 'src/app/models/Restaurant';
 
 @Component({
@@ -12,6 +12,7 @@ export class RestaurantListComponent {
    * Member Variables
    */
 
+  @Input()
   restaurants : Restaurant[] = [];
 
 
--- src/app/pages/list-of-restaurants/list-of-restaurants.component.ts
+++ src/app/pages/list-of-restaurants/list-of-restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Restaurant } from 'src/app/models/Restaurant';
 
 @Component({
   selector: 'app-list-of-restaurants',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-of-restaurants.component.css']
 })
 export class ListOfRestaurantsComponent {
+
+  /**
+   * Member Variables
+   */
+
+  restaurants : Restaurant[] = [
+    {
+    userId : 0,
+    userName : "test username",
+    password : "test password",
+    restaurantId : 0,
+    restaurantName : "Test Restaurant"
+    },
+    {
+      userId : 0,
+      userName : "test username",
+      password : "test password",
+      restaurantId : 0,
+      restaurantName : "Test Restaurant 2"
+      },
+      {
+        userId : 0,
+        userName : "test username",
+        password : "test password",
+        restaurantId : 0,
+        restaurantName : "Test Restaurant 3"
+        }
+  ];
+
+
+
+  /**
+   * Constructor
+   */
+
+  constructor() { }
+
+
+
+  /**
+   * Class Methods
+   */
+
+
 
 }
