a4217357ca9c8a3325355511a940e7283f6dbb23
austinalex1995 <aealex1995@gmail.com>
2023-03-23 15:54:04 -0400

    passed data to restaurant details from a binded property and displayed the data

--- src/app/pages/restaurant-details/restaurant-details.component.ts
+++ src/app/pages/restaurant-details/restaurant-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Reservation } from 'src/app/models/Reservation';
+import { Restaurant } from 'src/app/models/Restaurant';
 
 @Component({
   selector: 'app-restaurant-details',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant-details.component.css']
 })
 export class RestaurantDetailsComponent {
+
+  /**
+   * Member Variables
+   */
+
+  selectedRestaurant : Restaurant = {
+    userId : 0,
+    userName : "test username",
+    password : "test password",
+    restaurantId : 0,
+    restaurantName : "Test Restaurant"
+  }
+  availableReservations : Reservation[] = [
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "5:00 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "5:30 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "6:00 PM"
+    },
+    {
+      reservationId : 0,
+      userId : 0,
+      restaurantId : 0,
+      time : "6:30 PM"
+    }
+  ];
+
+
+
+  /**
+   * Constructor
+   */
+
+  constructor() { }
+
+
+
+  /**
+   * Class Methods
+   */
+
+
 
 }
