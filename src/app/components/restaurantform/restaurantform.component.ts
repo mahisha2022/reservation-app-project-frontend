f4027f1382bba6a6535a4434121d318ecaa55dd2
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-03-28 09:58:25 -0400

    restaurant reg form added

---
+++ src/app/components/restaurantform/restaurantform.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from 'src/app/models/Restaurant';
+import { RestaurantService } from 'src/app/services/restaurant.service';
+
+@Component({
+  selector: 'app-restaurantform',
+  templateUrl: './restaurantform.component.html',
+  styleUrls: ['./restaurantform.component.css']
+})
+export class RestaurantformComponent implements OnInit{
+  restaurants: Restaurant[] = [];
+  name: string = "";
+  address: string = "";
+  phone: string = "";
+  openAt: string = "";
+  closeAt: string = "";
+  totalSeats: number = 0;
+
+  constructor(private restaurantService: RestaurantService){}
+  ngOnInit() {
+    this.getRestaurants();
+  }
+
+  getRestaurants() {
+    this.restaurantService.getRestaurant()
+      .subscribe((restaurants: Restaurant[]) => {
+        this.restaurants = restaurants;
+      });
+  }
+
+  submit(): void{
+    let restaurant: Restaurant = {
+      restaurantName: this.name, address: this.address, phone: this.phone, 
+      openAt: this.openAt, closeAt: this.closeAt, totalSeats: this.totalSeats
+    };
+    this.restaurantService.postRestaurant(restaurant).subscribe();
+  }
+
+}
