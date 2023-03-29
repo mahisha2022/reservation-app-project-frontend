6b96182bf3753ce7c111c4a9c42c7a6c38dedef0
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-03-28 22:49:18 -0400

    reservation input component modified

--- src/app/components/reservation-input/reservation-input.component.ts
+++ src/app/components/reservation-input/reservation-input.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from 'src/app/models/Reservation';
+import { ReservationsAppServiceService } from 'src/app/services/reservations-app-service.service';
 
 @Component({
   selector: 'app-reservation-input',
   templateUrl: './reservation-input.component.html',
   styleUrls: ['./reservation-input.component.css']
 })
-export class ReservationInputComponent {
+export class ReservationInputComponent implements OnInit{
+   
 
+     reservationDate:string = "";
+     guestNum:number = 0;
+     specialAccomodation: string = "";
+
+     constructor(private reservationService: ReservationsAppServiceService){}
+
+     ngOnInit(): void {}
+    
+
+    submit(): void{
+        let reservation: Reservation = {
+          reservationDate: this.reservationDate, guestNum: this.guestNum, specialAccomodation: this.specialAccomodation
+        };
+        this.reservationService.postReservation(reservation).subscribe();
+    }
 }
--- src/app/models/Reservation.ts
+++ src/app/models/Reservation.ts
@@ -1,6 +1,10 @@
 export interface Reservation {
-    reservationId : number,
-    userId : number,
-    restaurantId : number,
-    time : string
+    // reservationId : number,
+    // userId : number,
+    // restaurantId : number,
+    // time : string
+
+    reservationDate: string,
+    guestNum: number,
+    specialAccomodation: string;
 }
